0754e5a2840e2bb1fec2b839c6170d40582cc6b7
Lord Savith <bulletproofchild@gmail.com>
2025-07-31 15:47:31 -0600

    How to create a debug line

--- UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Private/Items/Item.cpp
+++ UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Private/Items/Item.cpp
@@ -15,35 +15,17 @@ void AItem::BeginPlay()
 {
     Super::BeginPlay();
 
-    UE_LOG(LogTemp, Warning, TEXT("BeginPlay (Item)"))
-
-    if (GEngine)
-    {
-        GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("BeginPlay (Item)"));
-    }
-
     UWorld* World = GetWorld();
+    FVector Location = GetActorLocation();
+    FVector Forward = GetActorForwardVector();
+    FVector Length = Location + Forward * 100.f;
 
-    if (World)
-    {
-        FVector Location = GetActorLocation();
-        DRAW_SPHERE(Location);
-    }
+    DRAW_SPHERE(Location);
+    DRAW_LINE(Location, Length);
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
-
-    UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-
-    if (GEngine)
-    {
-        FString Name = GetName();
-        FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-        GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-
-        UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-    }
 }
--- UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Ult_Game_Dev_RPG.h
+++ UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Ult_Game_Dev_RPG.h
@@ -8,3 +8,7 @@
 #define DRAW_SPHERE(Location)                                                                                          \
     if (GetWorld())                                                                                                    \
         DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+
+#define DRAW_LINE(StartLocation, EndLocation)                                                                          \
+    if (GetWorld())                                                                                                    \
+        DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
