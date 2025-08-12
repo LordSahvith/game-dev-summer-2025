61f79833206c0324db29dc45834bd8cfeb52da53
Lord Savith <bulletproofchild@gmail.com>
2025-08-11 22:53:44 -0600

    Start Weapon Item

--- UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Public/Items/Item.h
+++ UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Public/Items/Item.h
@@ -21,7 +21,7 @@ class ULT_GAME_DEV_RPG_API AItem : public AActor
     virtual void BeginPlay() override;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
-    float Amplitude = 0.25f;
+    float Amplitude = 0.5f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
     float TimeConstant = 5.f;
@@ -33,13 +33,13 @@ class ULT_GAME_DEV_RPG_API AItem : public AActor
     float TransformedCos();
 
     UFUNCTION()
-    void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                              UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-                              const FHitResult& SweepResult);
+    virtual void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                      const FHitResult& SweepResult);
 
     UFUNCTION()
-    void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+    virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
   private:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
