5cae4f9cf31e381613b1f21e7edd35e414eab22c
Lord Savith <bulletproofchild@gmail.com>
2025-08-14 00:14:55 -0600

    Create Breakable Actor Class

--- UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Ult_Game_Dev_RPG.Build.cs
+++ UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Ult_Game_Dev_RPG.Build.cs
@@ -8,7 +8,7 @@ public class Ult_Game_Dev_RPG : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "GeometryCollectionEngine" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
