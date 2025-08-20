9bb5e58ae0e52b83a70884f1ad39eb71578d41ac
Lord Savith <bulletproofchild@gmail.com>
2025-08-19 21:12:53 -0600

    Enemy: Create Base Blueprint Class

--- UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Private/Enemy/Enemy.cpp
+++ UE5/Ult_Game_Dev_RPG/Source/Ult_Game_Dev_RPG/Private/Enemy/Enemy.cpp
@@ -218,7 +218,7 @@ void AEnemy::MoveToTarget(AActor* Target)
 
     FAIMoveRequest MoveRequest;
     MoveRequest.SetGoalActor(Target);
-    MoveRequest.SetAcceptanceRadius(75.f);
+    MoveRequest.SetAcceptanceRadius(50.f);
 
     EnemyController->MoveTo(MoveRequest);
 }
