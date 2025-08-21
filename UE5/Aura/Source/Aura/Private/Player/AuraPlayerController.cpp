9bb629e43004f4ad3ef98d088c6666cf9455c8b2
Lord Savith <bulletproofchild@gmail.com>
2025-08-21 14:51:35 -0600

    Input: Hook up Enhanced Input

--- UE5/Aura/Source/Aura/Private/Player/AuraPlayerController.cpp
+++ UE5/Aura/Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,2 +1,31 @@
 // Copyright Lord Savith
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+    bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Assert Variable is Set
+    check(AuraContext);
+
+    UEnhancedInputLocalPlayerSubsystem* Subsystem =
+        ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+    check(Subsystem);
+
+    Subsystem->AddMappingContext(AuraContext, 0);
+
+    bShowMouseCursor = true;
+    DefaultMouseCursor = EMouseCursor::Default;
+
+    FInputModeGameAndUI InputModeData;
+    InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+    InputModeData.SetHideCursorDuringCapture(false);
+    SetInputMode(InputModeData);
+}
