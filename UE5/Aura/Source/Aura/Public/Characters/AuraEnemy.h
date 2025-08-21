bd18a2415d59cbb83596c9a245b68494954a0111
Lord Savith <bulletproofchild@gmail.com>
2025-08-21 15:42:27 -0600

    Enemy: Setup Highlight Interface

--- UE5/Aura/Source/Aura/Public/Characters/AuraEnemy.h
+++ UE5/Aura/Source/Aura/Public/Characters/AuraEnemy.h
@@ -3,10 +3,15 @@
 
 #include "CoreMinimal.h"
 #include "Characters/AuraCharacterBase.h"
+#include "Interactin/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
     GENERATED_BODY()
+
+  public:
+    virtual void HighlightActor() override;
+    virtual void UnHighlightActor() override;
 };
