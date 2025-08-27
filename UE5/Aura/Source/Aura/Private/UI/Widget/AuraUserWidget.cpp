2c6f7c91580294e9cff22d14c8a2be7051496b23
Lord Savith <bulletproofchild@gmail.com>
2025-08-26 23:57:13 -0600

    UI: Setting up the User Widgets

---
+++ UE5/Aura/Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,8 @@
+// Copyright Lord Savith
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+    WidgetController = InWidgetController;
+    WidgetControllerSet();
+}
