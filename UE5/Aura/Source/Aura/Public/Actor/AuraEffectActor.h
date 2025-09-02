57e49d9ebe8a9dbcd710da912ffcf0f54f9bc72c
Lord Savith <bulletproofchild@gmail.com>
2025-09-01 19:51:13 -0600

    Gameplay Effects: Duration

--- UE5/Aura/Source/Aura/Public/Actor/AuraEffectActor.h
+++ UE5/Aura/Source/Aura/Public/Actor/AuraEffectActor.h
@@ -24,5 +24,8 @@ class AURA_API AAuraEffectActor : public AActor
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
     TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+    TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
   private:
 };
