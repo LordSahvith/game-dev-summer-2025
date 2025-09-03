9ec1fababd76101d5aa91b44a0245f8c51bfeaa2
Lord Savith <bulletproofchild@gmail.com>
2025-09-02 21:12:54 -0600

    Gameplay Tags: Broadcast Data Table Rows

--- UE5/Aura/Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ UE5/Aura/Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -30,11 +30,15 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
         .AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
     Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)
-        ->EffectAssetTags.AddLambda([](const FGameplayTagContainer& AssetTags) {
+        ->EffectAssetTags.AddLambda([this](const FGameplayTagContainer& AssetTags) {
             for (const FGameplayTag& Tag : AssetTags)
             {
-                const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-                GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+                FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
+                if (Tag.MatchesTag(MessageTag))
+                {
+                    const FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
+                    MessageWidgetRowDelegate.Broadcast(*Row);
+                }
             }
         });
 }
--- UE5/Aura/Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ UE5/Aura/Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -10,11 +10,6 @@
 struct FOnAttributeChangeData;
 class UAuraUserWidget;
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
-
 USTRUCT(BlueprintType)
 struct FUIWidgetRow : public FTableRowBase
 {
@@ -32,6 +27,15 @@ struct FUIWidgetRow : public FTableRowBase
     UPROPERTY(EditAnywhere, BlueprintReadOnly)
     UTexture2D* Image = nullptr;
 };
+
+/**
+ * DELEGATES
+ */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 
 UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController
@@ -54,6 +58,9 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController
     UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
     FOnMaxManaChangedSignature OnMaxManaChanged;
 
+    UPROPERTY(BlueprintAssignable, Category = "GAS|Messages")
+    FMessageWidgetRowSignature MessageWidgetRowDelegate;
+
   protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
     TObjectPtr<UDataTable> MessageWidgetDataTable;
@@ -62,4 +69,13 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController
     void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
     void ManaChanged(const FOnAttributeChangeData& Data) const;
     void MaxManaChanged(const FOnAttributeChangeData& Data) const;
+
+    template <typename T>
+    T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
 };
+
+template <typename T>
+T* UOverlayWidgetController::GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag)
+{
+    return DataTable->FindRow<T>(Tag.GetTagName(), TEXT(""));
+}
