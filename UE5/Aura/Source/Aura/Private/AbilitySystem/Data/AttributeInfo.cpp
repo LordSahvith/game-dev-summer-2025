ae07ed259f89e5d191cb810dae026593e728555b
Lord Savith <bulletproofchild@gmail.com>
2025-09-09 00:55:31 -0600

    Attribute Menu: Data Asset

---
+++ UE5/Aura/Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,21 @@
+// Copyright Lord Savith
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotfound) const
+{
+    for (const FAuraAttributeInfo& Info : AttributeInformation)
+    {
+        if (Info.AttributeTag.MatchesTagExact(AttributeTag)) { return Info; }
+    }
+
+    if (bLogNotfound)
+    {
+        UE_LOG(LogTemp,
+               Error,
+               TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."),
+               *AttributeTag.ToString(),
+               *GetNameSafe(this));
+    }
+
+    return FAuraAttributeInfo();
+}
