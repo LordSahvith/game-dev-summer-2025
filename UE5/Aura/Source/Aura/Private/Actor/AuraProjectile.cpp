ecad15e546a7c09809fec0fa64bd09fc9b847841
Lord Savith <bulletproofchild@gmail.com>
2025-09-23 02:32:30 -0600

    Ability Tasks: Projectile Impact

--- UE5/Aura/Source/Aura/Private/Actor/AuraProjectile.cpp
+++ UE5/Aura/Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -2,6 +2,10 @@
 #include "Actor/AuraProjectile.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Sound/SoundBase.h"
+#include "Components/AudioComponent.h"
 
 AAuraProjectile::AAuraProjectile()
 {
@@ -26,7 +30,21 @@ void AAuraProjectile::BeginPlay()
 {
     Super::BeginPlay();
 
+    SetLifeSpan(LifeSpan);
     Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+    LoopingSoundComponent = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
+}
+
+void AAuraProjectile::Destroyed()
+{
+    if (!bHit && !HasAuthority())
+    {
+        UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+        UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+        if (LoopingSoundComponent->IsPlaying()) { LoopingSoundComponent->Stop(); }
+    }
+
+    Super::Destroyed();
 }
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
@@ -36,4 +54,11 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
                                       bool bFromSwweep,
                                       const FHitResult& SweepResult)
 {
+    UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+
+    if (LoopingSoundComponent->IsPlaying()) { LoopingSoundComponent->Stop(); }
+
+    if (HasAuthority()) { Destroy(); }
+    else { bHit = true; }
 }
--- UE5/Aura/Source/Aura/Public/Actor/AuraProjectile.h
+++ UE5/Aura/Source/Aura/Public/Actor/AuraProjectile.h
@@ -7,6 +7,9 @@
 
 class USphereComponent;
 class UProjectileMovementComponent;
+class UNiagaraSystem;
+class USoundBase;
+class UAudioComponent;
 
 UCLASS()
 class AURA_API AAuraProjectile : public AActor
@@ -21,6 +24,7 @@ class AURA_API AAuraProjectile : public AActor
 
   protected:
     virtual void BeginPlay() override;
+    virtual void Destroyed() override;
 
     UFUNCTION()
     void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
@@ -31,6 +35,23 @@ class AURA_API AAuraProjectile : public AActor
                          const FHitResult& SweepResult);
 
   private:
+    UPROPERTY(EditDefaultsOnly)
+    float LifeSpan = 15.f;
+
+    bool bHit = false;
+
     UPROPERTY(VisibleAnywhere)
     TObjectPtr<USphereComponent> Sphere;
+
+    UPROPERTY(EditAnywhere)
+    TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+    UPROPERTY(EditAnywhere)
+    TObjectPtr<USoundBase> ImpactSound;
+
+    UPROPERTY(EditAnywhere)
+    TObjectPtr<USoundBase> LoopingSound;
+
+    UPROPERTY()
+    TObjectPtr<UAudioComponent> LoopingSoundComponent;
 };
