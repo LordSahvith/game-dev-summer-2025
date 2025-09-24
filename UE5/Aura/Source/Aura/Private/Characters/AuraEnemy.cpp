a518274d7eace31f8a30062d82b5ce4136ff3b33
Lord Savith <bulletproofchild@gmail.com>
2025-09-24 00:57:47 -0600

    Ability Tasks: Enemy Health Bar

--- UE5/Aura/Source/Aura/Private/Characters/AuraEnemy.cpp
+++ UE5/Aura/Source/Aura/Private/Characters/AuraEnemy.cpp
@@ -4,6 +4,8 @@
 #include "Aura/Aura.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/WidgetComponent.h"
+#include "UI/Widget/AuraUserWidget.h"
 
 AAuraEnemy::AAuraEnemy()
 {
@@ -14,12 +16,35 @@ AAuraEnemy::AAuraEnemy()
     AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 
     AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("Attribute Set");
+
+    HealthBar = CreateDefaultSubobject<UWidgetComponent>("Health Bar");
+    HealthBar->SetupAttachment(GetRootComponent());
 }
 
 void AAuraEnemy::BeginPlay()
 {
     Super::BeginPlay();
     InitAbilityActorInfo();
+
+    // Set Progress Bar's Widget Controller
+    if (UAuraUserWidget* AuraUserWidget = Cast<UAuraUserWidget>(HealthBar->GetUserWidgetObject()))
+    {
+        AuraUserWidget->SetWidgetController(this);
+    }
+
+    if (const UAuraAttributeSet* AuraAS = Cast<UAuraAttributeSet>(AttributeSet))
+    {
+        // bind delegate to broadcast attribute change
+        AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAS->GetHealthAttribute())
+            .AddLambda([this](const FOnAttributeChangeData& Data) { OnHealthChanged.Broadcast(Data.NewValue); });
+
+        AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAS->GetMaxHealthAttribute())
+            .AddLambda([this](const FOnAttributeChangeData& Data) { OnMaxHealthChanged.Broadcast(Data.NewValue); });
+
+        // broadcast initial values
+        OnHealthChanged.Broadcast(AuraAS->GetHealth());
+        OnMaxHealthChanged.Broadcast(AuraAS->GetMaxHealth());
+    }
 }
 
 void AAuraEnemy::HighlightActor()
--- UE5/Aura/Source/Aura/Public/Characters/AuraEnemy.h
+++ UE5/Aura/Source/Aura/Public/Characters/AuraEnemy.h
@@ -4,7 +4,10 @@
 #include "CoreMinimal.h"
 #include "Characters/AuraCharacterBase.h"
 #include "Interaction/EnemyInterface.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
+
+class UWidgetComponent;
 
 UCLASS()
 class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
@@ -25,6 +28,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
      */
     virtual int32 GetPlayerLevel() override;
 
+    UPROPERTY(BlueprintAssignable)
+    FOnAttributeChangedSignature OnHealthChanged;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnAttributeChangedSignature OnMaxHealthChanged;
+
   protected:
     virtual void BeginPlay() override;
     virtual void InitAbilityActorInfo() override;
@@ -32,5 +41,6 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
     int32 Level = 1;
 
-  private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    TObjectPtr<UWidgetComponent> HealthBar;
 };
