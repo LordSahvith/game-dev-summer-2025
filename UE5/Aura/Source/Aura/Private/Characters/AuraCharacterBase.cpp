ea560d21fc1fd18d904ae494042fb7741348b0b8
Lord Savith <bulletproofchild@gmail.com>
2025-09-29 23:50:02 -0600

    Damage: Dissolve Enemies

--- UE5/Aura/Source/Aura/Private/Characters/AuraCharacterBase.cpp
+++ UE5/Aura/Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -43,6 +43,8 @@ void AAuraCharacterBase::MulticastHandleDeath_Implementation()
     GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 
     GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    Dissolve();
 }
 
 void AAuraCharacterBase::BeginPlay() { Super::BeginPlay(); }
@@ -80,4 +82,27 @@ void AAuraCharacterBase::AddCharacterAbilities()
 
     UAuraAbilitySystemComponent* AuraASC = CastChecked<UAuraAbilitySystemComponent>(AbilitySystemComponent);
     AuraASC->AddCharacterAbilities(StartupAbilities);
+}
+
+/**
+ * DISOLVE EFFECTS
+ */
+void AAuraCharacterBase::Dissolve()
+{
+    if (IsValid(DissolveMaterialInstance))
+    {
+        UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+        GetMesh()->SetMaterial(0, DynamicMatInst);
+
+        StartDissolveTimeline(DynamicMatInst);
+    }
+
+    if (IsValid(WeaponDissolveMaterialInstance))
+    {
+        UMaterialInstanceDynamic* DynamicMatInst =
+            UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+        Weapon->SetMaterial(0, DynamicMatInst);
+
+        StartWeaponDissolveTimeline(DynamicMatInst);
+    }
 }
--- UE5/Aura/Source/Aura/Public/Characters/AuraCharacterBase.h
+++ UE5/Aura/Source/Aura/Public/Characters/AuraCharacterBase.h
@@ -65,6 +65,23 @@ class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInte
 
     void AddCharacterAbilities();
 
+    /**
+     * DISOLVE EFFECTS
+     */
+    void Dissolve();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void StartDissolveTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void StartWeaponDissolveTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TObjectPtr<UMaterialInstance> DissolveMaterialInstance;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TObjectPtr<UMaterialInstance> WeaponDissolveMaterialInstance;
+
   private:
     UPROPERTY(EditAnywhere, Category = "Abilities")
     TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
