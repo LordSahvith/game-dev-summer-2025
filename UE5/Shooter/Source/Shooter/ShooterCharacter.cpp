87a0e4fe9c0c4f2f167f4faa0b14e1958609da92
Lord Savith <bulletproofchild@gmail.com>
2025-10-21 21:00:35 -0600

    Start Character

---
+++ UE5/Shooter/Source/Shooter/ShooterCharacter.cpp
@@ -0,0 +1,31 @@
+// Lord Savith - Shooter Course
+
+#include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AShooterCharacter::AShooterCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true;
+}
+
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
---
+++ UE5/Shooter/Source/Shooter/ShooterCharacter.h
@@ -0,0 +1,31 @@
+// Lord Savith - Shooter Course
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class USpringArmComponent;
+
+UCLASS()
+class SHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AShooterCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+};
