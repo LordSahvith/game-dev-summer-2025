15ffa371bf1fcf2784bd56b50329650b76f29389
Lord Savith <bulletproofchild@gmail.com>
2025-10-28 02:57:15 -0600

    Start Character

---
+++ UE5/Shooter_Character/Source/Shooter_Character/Shooter_Character.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Shooter_Character.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shooter_Character, "Shooter_Character" );
