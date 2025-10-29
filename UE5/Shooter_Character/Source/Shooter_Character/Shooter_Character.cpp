369b2a4179be2b82c9c4f7d3c32f50f9c8971a9f
Lord Savith <bulletproofchild@gmail.com>
2025-10-29 17:45:33 -0600

    remove stuff

--- UE5/Shooter_Character/Source/Shooter_Character/Shooter_Character.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Shooter_Character.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shooter_Character, "Shooter_Character" );
